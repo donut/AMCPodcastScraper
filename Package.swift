4598f7bff64c2a90c5dcb90c8c328256e31bdf3b
Donovan Mueller <zotobi@gmail.com>
2016-03-10 15:03:49 -0700

    Prepare for building with `swift build`

--- Package.swift
+++ Package.swift
@@ -11,8 +11,11 @@ import PackageDescription
 let package = Package(
 	name: "AMCPodcastScraper",
 	dependencies: [
-		.Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 2)
-		.Package(url: "https://github.com/Zewo/HTTPClient.git", majorVersion: 0, minor: 3)
-		.Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 2)
+		.Package(url: "https://github.com/Zewo/File.git",
+		         majorVersion: 0, minor: 2),
+		.Package(url: "https://github.com/Zewo/HTTPClient.git",
+		         majorVersion: 0, minor: 3),
+		.Package(url: "https://github.com/Zewo/JSON.git",
+		         majorVersion: 0, minor: 2)
 	]
 )
